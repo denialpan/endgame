229e1a510c43225547adbae90cba6f896455b6a8
Spagles <106791090+Spagles@users.noreply.github.com>
2026-08-01 14:17:32 +1000

    synchronize skybox layer pass with block entity rendering stage

--- src/main/java/com/ddd/endgame/dddsendgameClient.java
+++ src/main/java/com/ddd/endgame/dddsendgameClient.java
@@ -35,7 +35,7 @@ public class dddsendgameClient {
     }
 
     private static void onRenderLevelStage(RenderLevelStageEvent event) {
-        if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_ENTITIES) {
+        if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_BLOCK_ENTITIES) {
             EndgamePortalBlockEntityRenderer.renderSkyboxLayer(event);
         }
     }
