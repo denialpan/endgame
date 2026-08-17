395ce5d00121df93ba4cef6b7125c16a5e582cca
denial <danpan1232@gmail.com>
2026-08-17 08:51:04 -0400

    refactor god stick into "The Stick"

---
+++ src/main/java/com/ddd/endgame/TheStickRecipe.java
@@ -0,0 +1,36 @@
+package com.ddd.endgame;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.ItemLore;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.minecraft.world.item.enchantment.Enchantments;
+
+public final class TheStickRecipe {
+    private TheStickRecipe() {
+    }
+
+    public static ItemStack createResult(HolderLookup.Provider registries) {
+        ItemStack stack = new ItemStack(dddsendgame.THE_STICK.get());
+        registries.lookupOrThrow(Registries.ENCHANTMENT)
+                .listElements()
+                .filter(enchantment -> !enchantment.is(Enchantments.VANISHING_CURSE))
+                .forEach(enchantment -> stack.enchant(enchantment, Integer.MAX_VALUE));
+        ItemEnchantments enchantments = stack.get(DataComponents.ENCHANTMENTS);
+        if (enchantments != null) {
+            stack.set(DataComponents.ENCHANTMENTS, enchantments.withTooltip(false));
+        }
+
+        List<Component> lore = new ArrayList<>();
+        lore.add(Component.translatable("item.dddsendgame.the_stick.lore").withStyle(ChatFormatting.DARK_PURPLE));
+        stack.set(DataComponents.LORE, new ItemLore(lore));
+        stack.set(DataComponents.ITEM_NAME, Component.translatable("item.dddsendgame.the_stick").withStyle(ChatFormatting.LIGHT_PURPLE, ChatFormatting.BOLD));
+        return stack;
+    }
+}
---
+++ src/main/java/com/ddd/endgame/item/models/TheStickItemRenderer.java
@@ -0,0 +1,16 @@
+package com.ddd.endgame.item.models;
+
+import com.ddd.endgame.dddsendgame;
+import net.minecraft.resources.ResourceLocation;
+
+public class TheStickItemRenderer extends GeneratedStencilItemRenderer {
+    public static final TheStickItemRenderer INSTANCE = new TheStickItemRenderer();
+
+    private TheStickItemRenderer() {
+        super(
+                ResourceLocation.fromNamespaceAndPath(dddsendgame.MODID, "textures/item/the_stick.png"),
+                TheStickModel::originalModel,
+                "Unable to load endgame test stick texture masks"
+        );
+    }
+}
